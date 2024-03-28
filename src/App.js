acaf481fbcae939900ceae29f41034f51da05f30
kz3640 <kz3640@g.rit.edu>
2024-03-28 16:06:05 -0500

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.css';
 import HelloWorld from './Components/HelloWorld';
 import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
       {/* <HelloWorld name="Kai"/> */}
-      <CounterExample />
+      {/* <CounterExample /> */}
+      <Header />
+
     </div>
   );
 }
